68502e6336d008c449e24390e3f98a80a0bea369
R4bsusa1l <silasbaur0@gmail.com>
2025-03-25 22:57:31 +0100

    initial commit of sample kotlin whatsapp-kommunication app

---
+++ android_whatsapp_sample/MyApplication/app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.automatic_birthday_congratulator.sample"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.automatic_birthday_congratualtor.sample"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    // For androidx.activity (e.g., for ComponentActivity)
+    implementation(libs.androidx.activity.ktx)
+
+    // For androidx.compose (basic UI)
+    implementation(libs.ui)
+    // For androidx.compose (Material 3)
+    implementation(libs.material3)
+    // For androidx.compose (UI Preview)
+    implementation(libs.ui.tooling.preview)
+    // For androidx.compose (UI Test)
+    androidTestImplementation(libs.ui.test.junit4)
+    // For androidx.compose (UI Tooling)
+    debugImplementation(libs.ui.tooling)
+}
