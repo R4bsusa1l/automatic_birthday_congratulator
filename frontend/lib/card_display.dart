6c2462f4a2d6bcafa925464e3103d1058dedec37
R4bsusa1l <silasbaur0@gmail.com>
2025-03-31 21:28:52 +0200

    ..dat

---
+++ frontend/lib/card_display.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+import 'package:english_words/english_words.dart';
+
+class CardDisplay extends StatelessWidget {
+  const CardDisplay({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      elevation: 12,
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
