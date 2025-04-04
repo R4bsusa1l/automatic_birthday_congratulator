46c05e573386829e8ada105e3e242221f898e99e
R4bsusa1l <silasbaur0@gmail.com>
2025-04-04 22:45:22 +0200

    addition of favorites and generator

---
+++ frontend/lib/favorites_page.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+import 'package:provider/provider.dart';
+import 'package:automatic_birthday_congratulator/app_state.dart';
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('you have ${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
